2dff179747d4755937cca07fd79bb33329b4da01
Kaushtuv <kaushtuvgurung@gmail.com>
2015-06-16 17:30:28 +1000

    RMC updates

--- wp-content/themes/rmc/index.php
+++ wp-content/themes/rmc/index.php
@@ -6,27 +6,28 @@ get_header();
 ?>
 <div class="container-fluid">
     <div class="container home-wrapper">
-        <div id="slider" class="carousel slide" data-ride="carousel">
+        <div id="slider" class="carousel slide" data-ride="carousel" data-interval="false">
             <div class="carousel-inner" role="listbox">
                 <div class="item active">
                     <div class="item-text">
-                        <h1>Austrlia's Pemier Choice For Plumbing Products</h1>
-                        <h2>World Leaders In Water Control Valves and Push-Fit Plumbing Fittings</h2>
+                        <h1>Australia's Premier Choice <br>For Plumbing Products</h1>
+                        <h2>World Leaders In Water Control Valves <br>and Push-Fit Plumbing Fittings</h2>
                     </div>
-                    <img src="<?php echo bloginfo('template_directory') . '/img/HomePage.jpg'; ?>"/>
+                    <img src="<?php echo bloginfo('template_directory') . '/img/banner1.jpg'; ?>"/>
                 </div>
                 <div class="item">
-                    
-                    <img src="<?php echo bloginfo('template_directory') . '/img/HomePage.jpg'; ?>"/>
+                    <div class="item-text">
+                        <h1>The Worlds Choice For<br>Push-Fit Plumbing Fittings</h1>
+                        <h2>Controlling the flow of water the world<br>over for more than 30 years</h2>
+                    </div>
+                    <img src="<?php echo bloginfo('template_directory') . '/img/banner2.jpg'; ?>"/>
                 </div>
             </div>
             <a class="left carousel-control" href="#slider" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left left-arrow" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
+                <span class="slide-btn"><i class="fa fa-caret-left"></i></span>
             </a>
             <a class="right carousel-control" href="#slider" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right right-arrow" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
+                <span class="slide-btn"><i class="fa fa-caret-right"></i></span>
             </a>
         </div>
     </div>
@@ -39,7 +40,7 @@ get_header();
     </div>
     <div class="container">
         <div class="jump-points-header">
-            <h2>Browse Product By Category</h2>
+            <h2>Browse Products By Category</h2>
         </div>
         <div class="jump-points">
 
